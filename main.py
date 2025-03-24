f5bcd7e46d21b190e438fdedcf550c5bb40445ed
kushalpanchal100 <kush70578@gmail.com>
2025-03-24 18:54:19 +0530

    commit

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+
+import smtplib
+
+my_email = "kush70578@gmail.com"
+password = "zyjx yrpq oxjg wwot"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email, 
+        to_addrs="kushalpanchal2323@gmail.com", 
+        msg="Subject:Hello\n\nThis is body of my email")
+
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_weeks = now.weekday()
+print(day_of_weeks)
+
+date_of_birth = dt.datetime(year=2002, month=11, day=23)
+print(date_of_birth)
+
+connection.close()
